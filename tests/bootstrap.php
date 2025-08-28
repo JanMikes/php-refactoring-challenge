2f4e99c671ac73526eb1ccdd170d837d035d5923
Jan Mikeš <j.mikes@me.com>
2025-08-28 03:03:14 +0200

    CI: Debugging env vars

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,10 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+print_r($_ENV['MYSQL_DATABASE']);
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..', '.env.test');
 $dotenv->safeLoad();
+
+print_r($_ENV['MYSQL_DATABASE']);
+die();
